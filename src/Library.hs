7902c2b8e3fa3f07032d9e5928c9aa243c55bc84
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2021-06-04 09:54:56 -0300

    v1

--- src/Library.hs
+++ src/Library.hs
@@ -1,2 +1,65 @@
 module Library where
 import PdePreludat
+
+--Tripulantes
+data Tripulante = UnTripulante {
+    energia::Number
+} deriving (Show,Eq)
+
+enfrentarEsqueleto tripulante
+    |estaEnLasUltimas tripulante = perderEnergia 20 tripulante
+    |otherwise = perderEnergia 10 tripulante
+
+estaEnLasUltimas (UnTripulante energia) = energia < 50
+
+perderEnergia valor (UnTripulante energia)
+    |siLlegaACeroEnergia energia valor =  UnTripulante 0
+    |otherwise = UnTripulante (energia-valor)
+
+siLlegaACeroEnergia energia valor = energia<=valor
+
+transportarCarga peso tripulante = perderEnergia peso tripulante
+
+beberGrog tripulante = aumentarEnergia 20 tripulante
+
+aumentarEnergia valor (UnTripulante energia) = UnTripulante (energia+valor)
+
+tripulanteEstaMuerto (UnTripulante energia) = energia==0
+
+-- Barcos
+
+data Barco = UnBarco{
+    tamanio::Number,
+    tripulantes::[Tripulante],
+    oro::Number,
+    tablones::Number,
+    balas::Number
+} deriving (Show,Eq)
+
+--(UnBarco tamanio tripulantes oro tablones balas)
+galeon = UnBarco 150
+
+bergantin = UnBarco 100
+
+balandro = UnBarco 50
+
+esBarcoFantasma barco = noTieneTripulacion barco || tripulantesMuertos barco
+
+noTieneTripulacion (UnBarco _ tripulantes _ _ _) = tripulantes==[]
+
+tripulantesMuertos (UnBarco _ tripulantes _ _ _) = (todosMuertos . map tripulanteEstaMuerto) tripulantes
+
+todosMuertos tripulantes = all (==estarMuerto) tripulantes
+
+estarMuerto=True
+
+llenarBarco barco = (llenarConOro . balasPorPersona) barco
+
+balasPorPersona (UnBarco tamanio tripulantes oro tablones balas) = UnBarco tamanio tripulantes oro tablones (tresPorTripulante tripulantes)
+
+tresPorTripulante tripulantes = 3 * length tripulantes
+
+llenarConOro (UnBarco tamanio tripulantes oro tablones balas) = UnBarco tamanio tripulantes (7*tamanio) tablones balas
+
+
+
--- src/Spec.hs
+++ src/Spec.hs
@@ -4,11 +4,34 @@ import Library
 import Test.Hspec
 import Control.Exception (evaluate)
 
+tripulante1 = UnTripulante 60
+tripulante2 = UnTripulante 30
+tripulante3 = UnTripulante 0
+
+
 correrTests :: IO ()
 correrTests = hspec $ do
-   describe "Parcial" $ do
-     it "Test de ejemplo" $ do
-       2 + 2 `shouldBe` 4
+  describe "tripulantes" $ do
+    it "enfrento a un esqueleto estando en las buenas" $ do
+      enfrentarEsqueleto tripulante1 `shouldBe` UnTripulante 50
+    it "enfrento a un esqueleto estando en las ultimas" $ do
+      enfrentarEsqueleto tripulante2 `shouldBe` UnTripulante 10
+    it "transportar una carga que me deje con energia" $ do
+      transportarCarga 30 tripulante1 `shouldBe` UnTripulante 30
+    it "transportar una carga que deje sin energia" $ do
+      transportarCarga 40 tripulante2 `shouldBe` UnTripulante 0
+    it "beber grog aumenta 20 de energia en cualquier persona viva" $ do
+      beberGrog tripulante1 `shouldBe` UnTripulante 80
+    it "beber grog en una persona muerta" $ do
+      beberGrog tripulante3 `shouldBe` UnTripulante 20
+    it "saber si esta muerta una persona viva deberia dar false" $ do
+      tripulanteEstaMuerto tripulante1 `shouldBe` False
+    it "saber si esta muerta una persona muerta deberia dar true" $ do
+      tripulanteEstaMuerto tripulante3 `shouldBe` True
+  describe "tripulantes" $ do
+    it "enfrento a un esqueleto estando en las buenas" $ do
+      enfrentarEsqueleto tripulante1 `shouldBe` UnTripulante 50
+
 
 escribime :: Expectation
 escribime = implementame
