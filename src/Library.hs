91e745b8af536cdfe724cb3369a9f2c9c780878b
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2021-06-04 11:55:37 -0300

    12hs

--- src/Library.hs
+++ src/Library.hs
@@ -45,11 +45,11 @@ bergantin = UnBarco 100
 
 balandro = UnBarco 50
 
-esBarcoFantasma barco = noTieneTripulacion barco || tripulantesMuertos barco
+esBarcoFantasma barco = noTieneTripulacion barco || tieneTripulantesMuertos barco
 
 noTieneTripulacion (UnBarco _ tripulantes _ _ _) = tripulantes==[]
 
-tripulantesMuertos (UnBarco _ tripulantes _ _ _) = (todosMuertos . map tripulanteEstaMuerto) tripulantes
+tieneTripulantesMuertos (UnBarco _ tripulantes _ _ _) = (todosMuertos . map tripulanteEstaMuerto) tripulantes
 
 todosMuertos tripulantes = all (==estarMuerto) tripulantes
 
@@ -76,14 +76,63 @@ tablonesBarco (UnBarco _ _ _ tablones _) = tablones
 
 balasBarco (UnBarco _ _ _ _ balas) = balas
 
-tripulantesVivos tripulantes = (filter (/=0) . map obtenerEnergia) tripulantes
+oroBarco (UnBarco _ _ oro _ _) = oro
+
+tripulantesBarco (UnBarco _ tripulantes _ _ _) = tripulantes
+
+--tripulantesVivos tripulantes = (filter (/=0) . map obtenerEnergia) tripulantes
+data Vida = Vivo | Muerto deriving (Show,Eq)
+
+tripulantesVivos [] = []
+tripulantesVivos (tripulante:tripulantes)
+    |tripulanteEstaMuerto tripulante = tripulantesVivos tripulantes
+    |otherwise = tripulante:tripulantesVivos tripulantes
+
+tripulantesMuertos [] = []
+tripulantesMuertos (tripulante:tripulantes)
+    |tripulanteEstaMuerto tripulante = tripulante:tripulantesVivos tripulantes
+    |otherwise = tripulantesVivos tripulantes
+
 
 tripulantesConVida (UnBarco _ tripulantes _ _ _) = (length . tripulantesVivos) tripulantes
 
+
 ganaMasSegun frecurso barco barcoenemigo
-    |frecurso barco >= frecurso barcoenemigo = barcoLeGanoA barco barcoenemigo
-    |otherwise = barcoLeGanoA barcoenemigo barco
+    |frecurso barco >= frecurso barcoenemigo = ganarEnfrentamiento barco barcoenemigo
+    |otherwise = perderEnfrentamiento barco
+
+
+--Si el barco pierde contra su oponente se queda sin recursos. En caso contrario, se lleva consigo todos los recursos del otro barco 
+ganarEnfrentamiento (UnBarco tamanio tripulantes oro tablones balas) barcoperdedor = UnBarco tamanio tripulantes (oro+oroBarco barcoperdedor) (tablones+tablonesBarco barcoperdedor) (balas+balasBarco barcoperdedor)
+
+perderEnfrentamiento (UnBarco tamanio tripulantes _ _ _) = UnBarco tamanio tripulantes 0 0 0
+
+--Sucesos
+
+tripulantesCambiados tripulantesnuevos (UnBarco tamanio tripulantes oro tablones balas) = UnBarco tamanio tripulantesnuevos oro tablones balas
+
+embarcarTesoro peso (UnBarco tamanio tripulantes oro tablones balas) = UnBarco tamanio (distribuirTesoro peso tripulantes) (oro+peso) tablones balas
+
+
+distribuirTesoro peso tripulantes = (map (transportarCarga (div peso (tripulantesConVida (UnBarco 1 tripulantes 1 1 1)))) . tripulantesVivos) tripulantes -- puse 1s porque tengo que mandar un barco
+
+--enfrentarAUnBarco seria igual a la funcion "enfrentamiento" definida con anterioridad
+
+cargamentoDeGrog barco = tripulantesCambiados (bebidoCincoGrogs barco) barco
+
+bebidoCincoGrogs barco = (map(beberGrog.beberGrog.beberGrog.beberGrog.beberGrog) . tripulantesBarco) barco
+
+ejercitoDeEsqueletos esqueletos barco = (lucharEsqueletos esqueletos . head . tripulantesVivos . tripulantesBarco) barco
+
+lucharEsqueletos esqueletos tripulante = take (esqueletos+1) (iterate enfrentarEsqueleto tripulante)
+
+tiendaDeGrog barco
+    | esBarcoFantasma barco && oroBarco barco>=30 =  aplicarPrimerMuerto barco
+
+primerMuerto = head . tripulantesMuertos
+
+aplicarPrimerMuerto (UnBarco tamanio tripulantes oro tablones balas) = ((beberGrog . primerMuerto) tripulantes):filter (==(primerMuerto tripulantes)) tripulantes
 
 
 
-barcoLeGanoA barco barcoperdedor = 
+
--- src/Spec.hs
+++ src/Spec.hs
@@ -9,6 +9,7 @@ tripulante2 = UnTripulante 30
 tripulante3 = UnTripulante 0
 
 barco1=UnBarco 100 [tripulante1,tripulante2] 10 5 20
+barco2=UnBarco 150 [tripulante1,tripulante3] 10 10 10
 
 correrTests :: IO ()
 correrTests = hspec $ do
@@ -38,7 +39,25 @@ correrTests = hspec $ do
       esBarcoFantasma barco1{tripulantes=[tripulante3]} `shouldBe` True
     it "lleno un barco" $ do
       llenarBarco barco1 `shouldBe` UnBarco 100 [tripulante1,tripulante2] 700 5 6
-    
+  describe "enfrentamientos" $ do
+    it "Un barco de mayor tamaño lucha contra otro de menor tamaño y al tener igual cant de balas gana" $ do
+      enfrentamiento barco2{balas=20} barco1 `shouldBe` UnBarco 150 [tripulante1,tripulante3] 20 15 40
+    it "Un barco de mayor tamaño lucha contra otro de menor tamaño y pierde por tener menos balas" $ do
+      enfrentamiento barco2 barco1 `shouldBe` UnBarco 150 [tripulante1,tripulante3] 0 0 0
+    it "un barco de menor tamaño lucha contra otro de mayor tamaño y gana por tener mas tripulantes vivos" $ do
+      enfrentamiento barco1 barco2 `shouldBe` UnBarco 100 [tripulante1,tripulante2] 20 15 30
+    it "un barco de menor tamaño lucha contra otro de mayor tamaño y pierde por tener menos gente viva" $ do
+      enfrentamiento barco1{tripulantes=[]} barco2 `shouldBe` UnBarco 100 [] 0 0 0
+    it "barcos de igual tamaño luchan y el primero gana por tener mas tablones de madera" $ do
+      enfrentamiento barco1{tamanio=150,tablones=15} barco2 `shouldBe` UnBarco 150 [tripulante1,tripulante2] 20 25 30
+    it "barcos de igual tamaño luchan pero el primero pierde por tener menos tablones de madera" $ do
+      enfrentamiento barco1{tamanio=150} barco2 `shouldBe` UnBarco 150 [tripulante1,tripulante2] 0 0 0
+  describe "sucesos" $ do
+    it "embarco un tesoro y me da el resultado" $ do
+      embarcarTesoro 10 barco1 `shouldBe` UnBarco 100 [tripulante1{energia=50},tripulante2{energia=20}] 20 5 20
+    it "encuentro un cargamento de grog y todos los tripulantes toman 5 grogs" $ do
+      cargamentoDeGrog barco1 `shouldBe` UnBarco 100 [tripulante1{energia=160},tripulante2{energia=130}] 10 5 20
+    it "enfrento a 3 esqueletos con un barco y el tripulante termina vivo"
 
 escribime :: Expectation
 escribime = implementame
